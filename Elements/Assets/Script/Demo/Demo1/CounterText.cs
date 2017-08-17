0f103b4f9f1c426d4312d7ca3ecdf8548dd7ecb6
Yuusuke Baba <yuusuke052@gmail.com>
2017-08-17 14:23:58 +0900

    Unityプロジェクトを追加

---
+++ Elements/Assets/Script/Demo/Demo1/CounterText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;	// 追加
+
+public class CounterText : MonoBehaviour {
+	private int countTime = 0;
+	public Text counterText;　// publicで変数宣言するとエディター上でいじれる
+
+	// 最初からある
+	// Use this for initialization
+	void Start () {
+		counterText.text = "0";
+	}
+
+	// 最初からある
+	// Update is called once per frame
+	void Update () {
+		if(countTime >= 50){
+			countTime = 0;
+			CountUp();
+		}
+		countTime ++;
+	}
+
+	void CountUp (){
+		int num = int.Parse(counterText.text);
+		counterText.text = (num + 1).ToString();
+	}
+}
