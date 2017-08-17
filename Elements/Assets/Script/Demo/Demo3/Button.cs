b6a91226e5074bc2fa344c379937e30c0f0e0d3b
Yuusuke Baba <yuusuke052@gmail.com>
2017-08-17 18:38:00 +0900

    Demo作成途中

---
+++ Elements/Assets/Script/Demo/Demo3/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン移動に必要
+
+namespace Demo3{ 
+	public class Button : MonoBehaviour {
+		public Scene nextScene;
+
+		public void Push(){
+			ChangeScene();
+		}
+
+		void ChangeScene(){
+			SceneManager.LoadScene("a");
+		}
+	}
+}
