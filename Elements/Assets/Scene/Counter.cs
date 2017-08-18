60fa590a01c86ba4c3f5a8aa3ab1a24ef08f0c06
sk0204 <k15053sk@apps.kct.ac.jp>
2017-08-18 10:15:09 +0900

    Collect

---
+++ Elements/Assets/Scene/Counter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;	// 追加
+
+public class Counter : MonoBehaviour
+{
+    public Text counterText; // publicで変数宣言するとエディター上でいじれる
+
+    // 最初からある
+    // Use this for initialization
+    void Start()
+    {
+        counterText.text = "0";
+    }
+}
